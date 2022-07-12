38427cecd76dc78bc468f9dd189f45549428c14f
nathalie.hsieh <nathalie.hsieh@ailabs.tw>
2022-07-12 16:06:01 +0800

    fix bugs for reading 10x files with prefix

--- script/single_cell_analysis.py
+++ script/single_cell_analysis.py
@@ -206,12 +206,13 @@ if args.format == 'csv':
     if args.input[-4:] != '.csv':
         sys.exit("The input file is not a CSV file.")
 elif args.format == '10x':
-    if not os.path.exists(os.path.join(args.input, 'matrix.mtx')) and not os.path.exists(os.path.join(args.input, 'matrix.mtx.gz')):
+    prefixes = f'{args.input}/{args.prefix}'
+    if not os.path.exists(f'{prefixes}matrix.mtx') and not os.path.exists(f'{prefixes}matrix.mtx.gz'):
         sys.exit("Cannot find 'matrix.mtx' file in the input directory.")
-    if not os.path.exists(os.path.join(args.input, 'genes.tsv')) and not os.path.exists(os.path.join(args.input, 'genes.tsv.gz')):
-        if not os.path.exists(os.path.join(args.input, 'features.tsv')) and not os.path.exists(os.path.join(args.input, 'features.tsv.gz')):
+    if not os.path.exists(f'{prefixes}genes.tsv') and not os.path.exists(f'{prefixes}genes.tsv.gz'):
+        if not os.path.exists(f'{prefixes}features.tsv') and not os.path.exists(f'{prefixes}features.tsv.gz'):
             sys.exit("Cannot find 'genes.tsv' or 'features.tsv' file in the input directory.")
-    if not os.path.exists(os.path.join(args.input, 'barcodes.tsv')) and not os.path.exists(os.path.join(args.input, 'barcodes.tsv.gz')):
+    if not os.path.exists(f'{prefixes}barcodes.tsv') and not os.path.exists(f'{prefixes}barcodes.tsv.gz'):
         sys.exit("Cannot find 'barcodes.tsv' file in the input directory.")
 elif args.format == 'h5ad':
     if args.input[-5:] != '.h5ad':
